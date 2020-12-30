e07493b16c0af1bed0202c2ec2736befeb02c93d
ankang <dreedisgood@qq.com>
2020-12-31 00:05:55 +0800

    基础案例

---
+++ parent/autodeliver/src/main/java/ankang/springcloud/AutoDeliverApplication.java
@@ -0,0 +1,24 @@
+package ankang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ankang
+ * @email: dreedisgood@qq.com
+ * @create: 2020-12-30
+ */
+@SpringBootApplication
+public class AutoDeliverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoDeliverApplication.class , args);
+    }
+
+
+    // 使用RestTemplate模板对象进行远程调用
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
---
+++ parent/autodeliver/src/main/java/ankang/springcloud/controller/AutoDeliverController.java
@@ -0,0 +1,35 @@
+package ankang.springcloud.controller;
+
+import ankang.springcloud.pojo.Resume;
+import ankang.springcloud.service.ResumeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: ankang
+ * @email: dreedisgood@qq.com
+ * @create: 2020-12-30
+ */
+@RestController
+@RequestMapping("/autodeliver")
+public class AutoDeliverController {
+
+    private final RestTemplate restTemplate;
+
+    public AutoDeliverController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/checkState/{userId}")
+    public Integer findResumeOpenState(@PathVariable Long userId) {
+        // 调用远程服务 -> 简历微服务接口 RestTemplate -> JdbcTemplate
+        // 以前httpclient封装好多内容进行远程调用
+        final Integer resumeOpenState = restTemplate.getForObject("http://localhost:8080/resume/openstate/" + userId,Integer.class)
+
+        return resumeOpenState;
+    }
+
+}
